4f9f74d77584d6a5e94b9907f8ae3336fa7fd16f
geomaas <geoff.maas@gmail.com>
2017-01-11 15:18:15 -0500

    actual styling and right git repository...

--- gulpfile.js
+++ gulpfile.js
@@ -26,7 +26,7 @@ gulp.task('css', function () {
 
 gulp.task('js', function () {
     return gulp.src('./js/app.js')
-        .pipe(browserify())
+        // .pipe(browserify())
         .pipe(gulp.dest('./public/js'))
 });
 
--- js/app.js
+++ js/app.js
@@ -1,6 +1,40 @@
-$(document).ready(function(){
+$(document).ready(function() {
+    /*------------------------------*/
+
+    // $('.title').addClass('animated slideInLeft');
+    // $('.mission').addClass('animated slideInDown');
+    // $('.about-craftsman').addClass('animated slideInUp');
+    // $('.gallery').addClass('animated slideInRight');
+
+    /*------------------------------*/
+
+    // $('.down-arrow').addClass('animated bounce');
+
+    /*------------------------------*/
+
+    // $(document).on('click', 'a[href^="#"]', function(e) {
+    //     // target element id
+    //     var id = $(this).attr('href');
+    //
+    //     // target element
+    //     var $id = $(id);
+    //     if ($id.length === 0) {
+    //         return;
+    //     }
+    //
+    //     // prevent standard hash navigation (avoid blinking in IE)
+    //     e.preventDefault();
+    //
+    //     // top position relative to the document
+    //     var pos = $(id).offset().top;
+    //
+    //     // animated top scrolling
+    //     $('body, html').animate({
+    //         scrollTop: pos
+    //     });
+    // });
+
+    /*------------------------------*/
 
 
-
-  
 });
